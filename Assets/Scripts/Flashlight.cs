27cf8783d3c4b8b184563c095e38a77232f1b638
MihailKanev01 <mihail.kanev.1995@abv.bg>
2025-01-27 19:36:53 +0200

    Added flashlight

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public GameObject flashlight;
+
+    public AudioSource turnOn;
+    public AudioSource turnOff;
+
+    private bool isOn = false;
+
+
+
+
+    void Start()
+    {
+        flashlight.SetActive(false);
+    }
+
+
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("F"))
+            ToggleFlashLight();
+    }
+
+    void ToggleFlashLight()
+    {
+        isOn = !isOn;
+        flashlight.SetActive(isOn);
+
+        if (isOn && turnOn != null)
+            turnOn.Play();
+        else
+            turnOff.Play();
+    }
+}
